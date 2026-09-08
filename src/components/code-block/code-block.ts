645ab368b5e9601d5cd7291f04cc881bda283ffa
Rhyan Eduardo <67023605+Rhyan-eduardo@users.noreply.github.com>
2026-09-08 20:45:06 -0300

    patch : shiki lib refacoted to singlenton instance

--- src/components/code-block/code-block.ts
+++ src/components/code-block/code-block.ts
@@ -1,25 +1,34 @@
 
-// CODE BLOCK [COMPONENT] : 
+// CODE BLOCK [COMPONENT] :
 
 import { UIBuilder } from "@limbusfoundation/uibuilder";
-import { codeToHtml } from "shiki";
+import { createHighlighterCore } from "shiki/core";
+import { createJavaScriptRegexEngine } from "shiki/engine/javascript";
+import typescript from "@shikijs/langs/typescript";
+import poimandres from "@shikijs/themes/poimandres";
 
-export const CodeBlock = UIBuilder.component( async ({ code } : { code : string})=> {
+const highlighter = await createHighlighterCore({
+    langs: [typescript],
+    themes: [poimandres],
+    engine: createJavaScriptRegexEngine()
+});
 
-    const codeContainer = UIBuilder.group({ className : "code-block"});
+export const CodeBlock = UIBuilder.component(async ({ code }: { code: string }) => {
 
-    const html = await codeToHtml(code, {
-        lang: "typescript",
+    const codeContainer = UIBuilder.group({ className: "code-block" });
+
+    const html = highlighter.codeToHtml(code, {
+        lang: "typescript", 
         theme: "poimandres"
-    });
+    }); 
 
-    const copyButton = UIBuilder.button({ label : "Copy" , className : "code-block-copy-button"});
+    const copyButton = UIBuilder.button({ label: "Copy", className: "code-block-copy-button" });
 
     UIBuilder.html.parseHTMLElement(codeContainer).innerHTML = html;
 
     codeContainer.render(copyButton);
 
-    UIBuilder.event(copyButton).add("click", async ()=>{
+    UIBuilder.event(copyButton).add("click", async () => {
         await navigator.clipboard.writeText(code);
         copyButton.label("Copied!");
         setTimeout(() => copyButton.label("Copy"), 1000);
@@ -28,4 +37,3 @@ export const CodeBlock = UIBuilder.component( async ({ code } : { code : string}
     return codeContainer;
 
 });
-
